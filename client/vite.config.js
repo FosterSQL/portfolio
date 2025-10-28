18dbbfa0ef40d4269f13bbee46b5bcecf2e3df43
FosterSQL <fostersquid@gmail.com>
2025-10-28 11:50:15 -0400

    server client simultaneus run fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/"
+  base: "/portfolio/",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
 })
